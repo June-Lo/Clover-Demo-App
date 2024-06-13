c24e310d2cc708f640de6a620e39533ba936d42f
June-Lo <92380506+June-Lo@users.noreply.github.com>
2024-06-13 15:38:18 -0400

    Update index.js

--- clover-server/index.js
+++ clover-server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const { redirect } = require('react-router-dom');
+cors = require('cors');
 
 require('dotenv').config({path: '../.env'});
 
 const app = express();
 const port = 3000;
+const portProxy = 3001;
+
+app.use(cors());
 
 const clientID = process.env.APP_ID;
 const clientSecret = process.env.APP_SECRET;
+const merchantID = process.env.JUNE_MID;
 
 app.get('/', (req, res) => {
-    res.redirect(`https://${process.env.CLOVER_SERVER}/oauth/authorize?client_id=${clientID}&redirect_uri=http://localhost:${port}/callback`);
+    res.redirect(`https://${process.env.CLOVER_SERVER}/oauth/authorize?client_id=${clientID}&merchant_id=${merchantID}&redirect_uri=http://localhost:${port}/callback`);
 });
 
 app.get('/callback', (req, res) => {
@@ -22,7 +27,8 @@ app.get('/callback', (req, res) => {
         employeeID,
         merchantID
     };
-    res.json(data);
+    // res.json(data);
+    res.redirect(`http://localhost:3001/?auth_code=${authCode}&employee_id=${employeeID}&merchant_id=${merchantID}`);
 });
 
 app.listen(port, () => {
