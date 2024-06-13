eb91b8be86e857c37422ce88563987f407714241
June-Lo <92380506+June-Lo@users.noreply.github.com>
2024-06-13 15:38:39 -0400

    Changes to Frontend

--- clover-frontend/src/App.js
+++ clover-frontend/src/App.js
@@ -1,12 +1,31 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  let queryParams = new URLSearchParams(window.location.search)
+  let clientID = queryParams.get('client_id');
+  let merchantID = queryParams.get('merchant_id');
+  let authCode = queryParams.get('auth_code');
+
+  useEffect(() => {
+    console.log("Client ID:", clientID);
+    //Redirect user to Clover login page if any of the parameters are empty
+    if (authCode === null) {
+      window.location.href = 'http://localhost:3000/';
+    }
+
+    queryParams = new URLSearchParams(window.location.search)
+    authCode = queryParams.get('auth_code');
+
+    console.log(clientID)
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const creditCardNumber = event.target.creditCardNumber.value;
     const expiryDate = event.target.expiryDate.value;
     const cvv = event.target.cvv.value;
-    
+
 
     console.log("Credit Card Number:", creditCardNumber);
     console.log("Expiry Date:", expiryDate);
