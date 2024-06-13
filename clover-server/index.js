657683a183af7b84508fe45c3e05fd53a87b5ac4
June-Lo <92380506+June-Lo@users.noreply.github.com>
2024-06-13 13:11:34 -0400

    Update index.js

--- clover-server/index.js
+++ clover-server/index.js
@@ -1,5 +1,8 @@
+const { redirect } = require('react-router-dom')
+
 require('dotenv').config()
 
 const clientID = process.env.APP_ID
 const clientSecret = process.env.APP_SECRET
 
+redirect("https://"+process.env.CLOVER_SERVER+"/oauth/authorize?client_id="+clientID+"&redirect_uri=http://localhost:3000")
