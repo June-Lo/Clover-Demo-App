2e25c5565b6c234bdcc61455534bcab368fca82e
June-Lo <92380506+June-Lo@users.noreply.github.com>
2024-06-13 13:30:16 -0400

    Added dotenv and Updated index.js

--- clover-server/index.js
+++ clover-server/index.js
@@ -1,8 +1,30 @@
-const { redirect } = require('react-router-dom')
+const express = require('express');
+const { redirect } = require('react-router-dom');
 
-require('dotenv').config()
+require('dotenv').config({path: '../.env'});
 
-const clientID = process.env.APP_ID
-const clientSecret = process.env.APP_SECRET
+const app = express();
+const port = 3000;
 
-redirect("https://"+process.env.CLOVER_SERVER+"/oauth/authorize?client_id="+clientID+"&redirect_uri=http://localhost:3000")
+const clientID = process.env.APP_ID;
+const clientSecret = process.env.APP_SECRET;
+
+app.get('/', (req, res) => {
+    res.redirect(`https://${process.env.CLOVER_SERVER}/oauth/authorize?client_id=${clientID}&redirect_uri=http://localhost:${port}/callback`);
+});
+
+app.get('/callback', (req, res) => {
+    const authCode = req.query.code;
+    const employeeID = req.query.employee_id;
+    const merchantID = req.query.merchant_id;
+    const data = {
+        authCode,
+        employeeID,
+        merchantID
+    };
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
